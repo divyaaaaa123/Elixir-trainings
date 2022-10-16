3823ee92a4fd2ab861383184e9088dce27e2b30d
divyaaaaa123 <47104889+divyaaaaa123@users.noreply.github.com>
2022-10-16 13:58:46 +0530

    Sum of 2 numbers by anonymous function and  checking whether that function exist and checking number of arguments in the function is 2

--- anonymousfn.exs
+++ anonymousfn.exs
@@ -4,3 +4,13 @@ add = fn a, b -> a + b end
 IO.puts(add.(1,2))
 
 IO.puts(is_function(add))
+
+# checking number of arguments in add function = 2 or no
+
+# 1. check if add is a function that expects exactly 2 arguments
+
+IO.puts(is_function(add,2))
+
+# 2. check if add is a function that expects exactly 1 argument
+
+IO.puts(is_function(add,1))
