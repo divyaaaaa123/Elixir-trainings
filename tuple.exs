b4a909cae69fd7f48caa1a74d4d822fd76b809e6
divyaaaaa123 <47104889+divyaaaaa123@users.noreply.github.com>
2022-10-16 17:48:11 +0530

    Tuple operations

---
+++ tuple.exs
@@ -0,0 +1,9 @@
+# Elixir uses curly brackets to define tuples. Like lists, tuples can hold any value:
+
+IO.inspect ({:ok,"hello"})
+
+tuple = {1, 2, 3 , :ok, "D"}
+IO.inspect (tuple_size(tuple))
+IO.inspect (elem(tuple, 4))   # It is also possible to put an element at a particular index in a tuple, by elem. index value start with 0
+
+
