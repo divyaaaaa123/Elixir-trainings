6106eaa128986791c2a13d161fc8bc7c5f8a815c
divyaaaaa123 <47104889+divyaaaaa123@users.noreply.github.com>
2022-10-16 15:12:01 +0530

    Sum of 2 numbers by anonymous function and checking whether that func… …tion exist and checking number of arguments in the function is 2 , etc.

--- anonymousfn.exs
+++ anonymousfn.exs
@@ -14,3 +14,15 @@ IO.puts(is_function(add,2))
 # 2. check if add is a function that expects exactly 1 argument
 
 IO.puts(is_function(add,1))
+
+#using above add function in another function
+
+twice = fn a -> add.(a,a) end
+
+IO.puts (twice.(4))
+
+# variable inside a function does not affect global declaration
+
+x = 14
+IO. puts (fn -> x = 21 end).()
+IO.puts (x)
