9102985b715a624de81cf6b89117eefff197cbb9
divyaaaaa123 <47104889+divyaaaaa123@users.noreply.github.com>
2022-10-16 17:39:35 +0530

    Linked list operations

--- linkedlist.exs
+++ linkedlist.exs
@@ -1,13 +1,14 @@
 # Elixir uses square brackets to specify a list of values. Values can be of any type:
-# List uses only ascii values to print
-IO.puts([104, 101, 108, 108, 111])                              # hello
-IO.puts (length [104, 101, 108, 108, 111])
-IO.puts([49,50,51,[116,114,117,101]]++[52, 53, 54])            # Concatanation: 123true456
 
-IO.puts([49,50,51,[116,114,117,101], 52, 53, 54]-- [[116,114,117,101]])      #Subtraction: 123456     
+IO.inspect([104, 101, 108, 108, 111])                              # hello
+IO.inspect (length [104, 101, 108, 108, 111])
+IO.inspect([49,50,51,[116,114,117,101]]++[52, 53, 54])            # Concatanation: 123true456
 
-list = [49, 50, 51]   
-IO.puts [hd(list)]  #head of the list --> 1
-IO.puts [tl(list)]  #tail of the list --> 23
+IO.inspect([49,50,51,[116,114,117,101], 52, 53, 54]-- [[116,114,117,101]])      #Subtraction: 123456
 
-IO.puts ('hello'== "hello")  # single quote charlist is not equal to double quote string  --> false
+list = [1, 2, 3]
+IO.inspect(list)        # [1,2,3]
+IO.inspect (hd(list))  #head of the list --> 1
+IO.inspect (tl(list))  #tail of the list --> 23
+
+IO.inspect ('hello'== "hello")  # single quote charlist is not equal to double quote string  --> false
