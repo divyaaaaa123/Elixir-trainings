6065e979b94f288943a80561bce0e045f1f31119
divyaaaaa123 <47104889+divyaaaaa123@users.noreply.github.com>
2022-10-16 17:26:50 +0530

    linked list operations

--- linkedlist.exs
+++ linkedlist.exs
@@ -1,7 +1,7 @@
 # Elixir uses square brackets to specify a list of values. Values can be of any type:
 # List uses only ascii values to print
 IO.puts([104, 101, 108, 108, 111])                              # hello
-
+IO.puts (length [104, 101, 108, 108, 111])
 IO.puts([49,50,51,[116,114,117,101]]++[52, 53, 54])            # Concatanation: 123true456
 
 IO.puts([49,50,51,[116,114,117,101], 52, 53, 54]-- [[116,114,117,101]])      #Subtraction: 123456     
