7bea1d6eae997f09b15847e8ac02ddf109ab2354
divyaaaaa123 <47104889+divyaaaaa123@users.noreply.github.com>
2022-10-16 11:25:03 +0530

    Basic Types : int, float, boolean, atom, string, escape

--- basictypes.exs
+++ basictypes.exs
@@ -1,7 +1,8 @@
-IO.puts(2+3)
-IO.puts(1.2+4.6)
-IO.puts(true)
-IO.puts(:atom)
-IO.puts("Hi, I am a string")
-
-
+IO.puts(2+3)                   #integer
+IO.puts(1.2+4.6)               #float
+IO.puts(true)                 #boolean
+IO.puts(:atom)                 # atom
+IO.puts("Hi, I am a string")   # string
+IO.puts(string = :D)   
+IO.puts("I am #{string}")      #string interpolation
+IO.puts("hello\nworld")        #escape
