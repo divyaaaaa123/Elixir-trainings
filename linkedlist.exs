7d8847aa5f12ee11bdd0b0171dc259ffc9ef69ba
divyaaaaa123 <47104889+divyaaaaa123@users.noreply.github.com>
2022-10-16 17:10:03 +0530

    linked list operations

--- linkedlist.exs
+++ linkedlist.exs
@@ -5,3 +5,7 @@ IO.puts([104, 101, 108, 108, 111])                              # hello
 IO.puts([49,50,51,[116,114,117,101]]++[52, 53, 54])            # Concatanation: 123true456
 
 IO.puts([49,50,51,[116,114,117,101], 52, 53, 54]-- [[116,114,117,101]])      #Subtraction: 123456     
+
+list = [49, 50, 51]   
+IO.puts [hd(list)]  #head of the list --> 1
+IO.puts [tl(list)]  #tail of the list --> 23
