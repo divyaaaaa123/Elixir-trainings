43bf8d046569783a01d6d5ee7d09894b76950812
divyaaaaa123 <47104889+divyaaaaa123@users.noreply.github.com>
2022-10-17 10:44:33 +0530

    Tuple operations

--- tuple.exs
+++ tuple.exs
@@ -5,5 +5,5 @@ IO.inspect ({:ok,"hello"})
 tuple = {1, 2, 3 , :ok, "D"}
 IO.inspect (tuple_size(tuple))
 IO.inspect (elem(tuple, 4))   # It is also possible to put an element at a particular index in a tuple, by elem. index value start with 0
-
+IO.inspect(put_elem(tuple,3,4))   #accessing tuple and adding 4 at index =3 
 
